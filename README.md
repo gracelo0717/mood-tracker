160808b33144792a4e77be4efbc020513b7638da
Grace <gracelo0717@gmail.com>
2024-06-12 16:04:43 -0400

    setup webpack

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # mood-tracker
+
 First scratch project
+
+// Steps:
+// 1. Setup webpack/dev server
+// 2. Tech stacks
+// 3. Print hello word
+// 4. MVP - Log moods + save feature
+// 5. Implement stretch features (if possible)
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: process.env.NODE_ENV || 'development',
+  entry: {
+    src: './client/index.js',
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'development',
+      template: 'index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.s?css/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      publicPath: '/build',
+      directory: path.resolve(__dirname, 'build'),
+    },
+    // frontend
+    port: 8080,
+    proxy: [
+      {
+        // backend
+        context: '/api',
+        target: 'http://localhost:3000',
+      },
+    ],
+  },
+};
